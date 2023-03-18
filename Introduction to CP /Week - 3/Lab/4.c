563e169807dad63ee946e374079decae60373fc4
Sivaa Balamurugan <74814237+SivaaB@users.noreply.github.com>
2023-03-18 10:34:47 +0400

    Update 4.c

--- Introduction to CP /Week - 3/Lab/4.c
+++ Introduction to CP /Week - 3/Lab/4.c
@@ -1,78 +1,41 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 int main (void)
 { 
-    int month_number; 
-
-    printf ("Enter month number (1-12): "); 
-    scanf ("%d", &month_number); 
-
-    switch (month_number)
-    { 
-        case 1 :
-        { 
-            printf ("January\n"); 
-            break; 
-        }
-        case 2 :
-        { 
-            printf ("February\n"); 
-            break; 
-        }
-        case 3 :
-        { 
-            printf ("March\n"); 
-            break; 
-        }
-        case 4 :
+    int month;
+    
+    printf("Plz input the number corresponding to the month :\n");
+    scanf("%d", &month);
+    
+    switch(month)
+    {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
         {
-            printf ("April\n"); 
-            break; 
+            printf("31\n");
+            break;
         }
-        case 5 :
+        case 4:
+        case 6:
+        case 9:
+        case 11:
         {
-            printf ("May\n"); 
-            break; 
+            printf("30\n");
+            break;
         }
-        case 6 :
+        case 2:
         {
-            printf ("June\n"); 
-            break; 
+            printf("28\n");
+            break;
         }
-        case 7 :
+        default:
         {
-            printf ("July\n"); 
-            break; 
-        }
-        case 8 :
-        {
-            printf ("August\n"); 
-            break; 
-        }
-        case 9 :
-        {
-            printf ("September\n"); 
-            break; 
-        }
-        case 10 :
-        {
-            printf ("October\n"); 
-            break; 
-        }
-        case 11 :
-        {
-            printf ("November\n"); 
-            break; 
-        }
-        case 12 :
-        {
-            printf ("December\n"); 
-            break; 
-        }
-        default: 
-        { 
-            printf ("Enter valid operand\n"); 
-            break; 
-        }
+            printf("Plz input the number corresponding to the month between 1 and 12: \n");
+        }    
     }
 }
