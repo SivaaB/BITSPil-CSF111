b67d01a565dd45ffb81cf5cfa90b159dc7d00449
Sivaa Balamurugan <74814237+SivaaB@users.noreply.github.com>
2023-05-13 08:52:20 +0400

    Update 4.c

--- Introduction to CP /Week - 07/4.c
+++ Introduction to CP /Week - 07/4.c
@@ -7,14 +7,14 @@ int main ()
 
     for (n = 0; n < row; n++)
     {
-        for (space = 1; space < row - n; space++)
+        for (space = 1; space < row - n; space++) // we want it to print spaces till row - n, and then eventually print the number
         { 
             printf (" "); 
         }
 
         for (r = 0; r <= n; r++)
         { 
-            if (n == 0 || r == 0) 
+            if (n == 0 || r == 0) // first element in pascal triangle is always bound to be 0
             { 
                 ncr = 1; 
                 printf ("%d ", ncr); 
@@ -22,10 +22,13 @@ int main ()
 
             else 
             { 
-                ncr = ncr * (n-r+1)/r; 
+                ncr = ncr * (n-r+1)/r; // ncr * (n - r - 1) / r
                 printf ("%d ", ncr); 
             }
         }
         printf ("\n"); 
     }
 }
+
+
+// PS : I hate this pattern. :(
